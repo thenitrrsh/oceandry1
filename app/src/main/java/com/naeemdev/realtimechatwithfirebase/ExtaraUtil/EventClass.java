fc8bec0da786891cb7d97a3cdadda8fc6f9a596e
Muhammad Naeem <naeem0313@gmail.com>
2019-12-02 20:43:16 +0500

    Feeds fragments with firbase data with  adpater

---
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/ExtaraUtil/EventClass.java
@@ -0,0 +1,10 @@
+package com.naeemdev.realtimechatwithfirebase.ExtaraUtil;
+
+public class EventClass {
+
+    public final String message;
+
+    public EventClass(String message) {
+        this.message = message;
+    }
+}
