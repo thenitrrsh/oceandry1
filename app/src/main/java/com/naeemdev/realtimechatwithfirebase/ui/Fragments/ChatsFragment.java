a91619ffd01a9460ff597f3bd9158f5e36365b69
Muhammad Naeem <naeem0313@gmail.com>
2019-12-02 18:31:58 +0500

    Create  Activity with fragments  for  Chats,  notification, matched

---
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/ui/Fragments/ChatsFragment.java
@@ -0,0 +1,38 @@
+package com.naeemdev.realtimechatwithfirebase.ui.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.naeemdev.realtimechatwithfirebase.R;
+
+
+/**
+ *
+ */
+public class ChatsFragment extends Fragment {
+
+
+    public ChatsFragment() {
+        // Required empty public constructor
+    }
+
+
+    /**
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat, container, false);
+    }
+
+
+}
