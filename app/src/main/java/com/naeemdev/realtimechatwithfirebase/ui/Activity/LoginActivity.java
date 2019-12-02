d23eed4c03df9d65f7ad50c16807445ac1afb99b
Muhammad Naeem <naeem0313@gmail.com>
2019-12-02 18:11:38 +0500

    Sign in  with email and facebook

---
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/ui/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.naeemdev.realtimechatwithfirebase.ui.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.naeemdev.realtimechatwithfirebase.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
