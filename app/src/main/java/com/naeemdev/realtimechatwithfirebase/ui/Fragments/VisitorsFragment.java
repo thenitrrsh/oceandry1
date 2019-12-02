e4e356f3f896af178ac6b2775f8e73de7b68842d
Muhammad Naeem <naeem0313@gmail.com>
2019-12-02 21:49:32 +0500

    Account Activity Matches, visitors, likes, favorites

---
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/ui/Fragments/VisitorsFragment.java
@@ -0,0 +1,22 @@
+package com.naeemdev.realtimechatwithfirebase.ui.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.naeemdev.realtimechatwithfirebase.R;
+
+public class VisitorsFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_visitores, container, false);
+    }
+
+}
