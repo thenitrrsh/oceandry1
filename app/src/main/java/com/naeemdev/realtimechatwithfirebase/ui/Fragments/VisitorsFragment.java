e90726a579403c398b11e38659ed1cc45807e58b
Muhammad Naeem <naeem0313@gmail.com>
2019-12-03 11:06:03 +0500

    Get Visitors  list  from firbase using firbase query create Visitors firbasebase adapter show list of Visitors if Visitors not found show message!

--- app/src/main/java/com/naeemdev/realtimechatwithfirebase/ui/Fragments/VisitorsFragment.java
+++ app/src/main/java/com/naeemdev/realtimechatwithfirebase/ui/Fragments/VisitorsFragment.java
@@ -1,22 +1,121 @@
 package com.naeemdev.realtimechatwithfirebase.ui.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.naeemdev.realtimechatwithfirebase.R;
+import com.naeemdev.realtimechatwithfirebase.firestore.VisitorsFirestore;
+import com.naeemdev.realtimechatwithfirebase.model.VisitorsClass;
+import com.naeemdev.realtimechatwithfirebase.ui.Activity.ProfileActivity;
 
 public class VisitorsFragment extends Fragment {
 
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+    FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+    LinearLayout linearLayoutVisitorsContent;
+    LinearLayout linearLayoutVisitorsEmpty;
+    private RecyclerView recyclerViewVisitsView;
+    private VisitorsFirestore visitorsFirestore;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_visitores, container, false);
+        View view = inflater.inflate(R.layout.fragment_visitores, container, false);
+
+        recyclerViewVisitsView = view.findViewById(R.id.recyclerViewVisitsView);
+
+        VisitsRecyclerView();
+
+
+        linearLayoutVisitorsContent = view.findViewById(R.id.linearLayoutVisitorsContent);
+        linearLayoutVisitorsContent.setVisibility(View.VISIBLE);
+        linearLayoutVisitorsEmpty = view.findViewById(R.id.linearLayoutVisitorsEmpty);
+        linearLayoutVisitorsEmpty.setVisibility(View.GONE);
+
+
+        firebaseFirestore.collection("users")
+                .document(firebaseUser.getUid())
+                .collection("visits")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                        if (queryDocumentSnapshots != null) {
+                            if (queryDocumentSnapshots.size() == 0) {
+
+                                linearLayoutVisitorsContent.setVisibility(View.GONE);
+                                linearLayoutVisitorsEmpty.setVisibility(View.VISIBLE);
+
+                            } else {
+
+                                linearLayoutVisitorsContent.setVisibility(View.VISIBLE);
+                                linearLayoutVisitorsEmpty.setVisibility(View.GONE);
+                            }
+                        }
+                    }
+                });
+
+
+        return view;
+    }
+
+    private void VisitsRecyclerView() {
+
+        Query query = firebaseFirestore.collection("users")
+                .document(firebaseUser.getUid())
+                .collection("visits")
+                .orderBy("user_visited", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<VisitorsClass> options = new FirestoreRecyclerOptions.Builder<VisitorsClass>()
+                .setQuery(query, VisitorsClass.class)
+                .build();
+
+        visitorsFirestore = new VisitorsFirestore(options);
+
+        recyclerViewVisitsView.setHasFixedSize(true);
+        recyclerViewVisitsView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerViewVisitsView.setAdapter(visitorsFirestore);
+
+        visitorsFirestore.setOnItemClickListener(new VisitorsFirestore.OnItemClickListener() {
+            @Override
+            public void onItemClick(DocumentSnapshot documentSnapshot, int position) {
+                final VisitorsClass visitorsClass = documentSnapshot.toObject(VisitorsClass.class);
+                String id = documentSnapshot.getId();
+                String path = documentSnapshot.getReference().getPath();
+
+                final Intent intent = new Intent(getContext(), ProfileActivity.class);
+                intent.putExtra("user_uid", visitorsClass.getUser_visitor());
+                startActivity(intent);
+
+            }
+        });
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        visitorsFirestore.startListening();
+
     }
 
 }
